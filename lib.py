16f34863785a42f4f81a8c2a75d43ec124c21294
Hari <hprasad2709@gmail.com>
2023-10-30 18:25:02 +0000

    file output enabled

--- lib.py
+++ lib.py
@@ -1,19 +1,15 @@
-PRECEDENCE = {
-    "¬" : 5,
-    "∧" : 4,
-    "∨" : 3,
-    "⇒" : 2,
-    "⇔" : 1
-}
+PRECEDENCE = {"¬": 5, "∧": 4, "∨": 3, "⇒": 2, "⇔": 1}
+
 
 def is_proposition(token: str) -> bool:
-    """Takes a token from a logical formula and returns whether that token is 
+    """Takes a token from a logical formula and returns whether that token is
     a valid propositional identifier. In the version of the logical language
     that this parser uses, a valid propositional identifier can only be a
     single alphabetical character."""
     if token.isalpha() and len(token) == 1:
         return True
     return False
+
 
 def is_operator(token: str) -> str:
     """Takes a token from a logical formula and returns whether that token is
@@ -22,12 +18,14 @@ def is_operator(token: str) -> str:
         return True
     return False
 
+
 def pad_zeros(num: str, length: int) -> str:
     """Takes a string representation of a binary number and pads it with zeros
     on the left side until it is the required `length`."""
     while len(num) != length:
         num = "0" + num
     return num
+
 
 def count_identifiers(exp: str) -> tuple[int, list]:
     """Takes a logical formula `exp` and counts the number of unique
@@ -42,6 +40,7 @@ def count_identifiers(exp: str) -> tuple[int, list]:
             identifiers.add(token)
 
     return (len(identifiers), sorted(list(identifiers)))
+
 
 def produce_truth_combinations(count: int, identifiers: list) -> list[dict]:
     """Takes a list of individual prime propositions and returns a list of
@@ -60,13 +59,14 @@ def produce_truth_combinations(count: int, identifiers: list) -> list[dict]:
                 combination[identifiers[i]] = False
             else:
                 combination[identifiers[i]] = True
-        
+
         truth_combinations.append(combination)
 
     return truth_combinations
 
+
 def shunting_yard(exp: str) -> str:
-    """Takes an infix propositional logic expression and returns the matrix 
+    """Takes an infix propositional logic expression and returns the matrix
     converted to postfix (Reverse Polish) notation."""
     # get rid of any spaces in the infix expression
     exp = exp.replace(" ", "")
@@ -83,8 +83,13 @@ def shunting_yard(exp: str) -> str:
         # higher-precedence operators from the stack are
         # popped off and pushed to the output queue
         elif is_operator(token):
-            while len(operator_stack) and operator_stack[-1] != "(" and PRECEDENCE[operator_stack[-1]] > PRECEDENCE[token]:
+            while (
+                len(operator_stack)
+                and operator_stack[-1] != "("
+                and PRECEDENCE[operator_stack[-1]] > PRECEDENCE[token]
+            ):
                 output_queue.append(operator_stack.pop())
+
             operator_stack.append(token)
 
         # left parens are directly pushed to the output queue
@@ -115,7 +120,7 @@ def shunting_yard(exp: str) -> str:
             # pop off and discard the left parens because they are unnecessary
             #  in RPN
             operator_stack.pop()
-    
+
     # any remaining operators get indiscriminately popped off and pushed to the
     #  output queue
     while len(operator_stack):
@@ -126,6 +131,53 @@ def shunting_yard(exp: str) -> str:
 
     return output_queue
 
+
+def get_subexps(exp: str, list_format=False) -> list:
+    """Takes a postfix (RP) expression and returns a list of smaller, simpler
+    propositions that occur within it, ending with the full expression. For
+    example, `'p q r ∧ ∨'` returns `['p', 'q', 'r', 'q ∧ r', 'p ∨ q ∧ r']`"""
+    eval_stack = list(exp)
+    current = []
+    subexps = {}
+    op_count = 0
+
+    while len(eval_stack) > 0:
+        if is_proposition(eval_stack[0]):
+            current.append(eval_stack.pop(0))
+
+        elif not is_operator(eval_stack[0]):
+            current.append(f"({eval_stack.pop(0)})")
+
+        else:
+            operator = eval_stack.pop(0)
+
+            if operator == "¬":
+                eval_stack.insert(0, f"¬{current[-1]}")
+                current = current[0:-1]
+            else:
+                match operator:
+                    case "∧":
+                        eval_stack.insert(0, f"{current[-2]} ∧ {current[-1]}")
+                    case "∨":
+                        eval_stack.insert(0, f"{current[-2]} ∨ {current[-1]}")
+                    case "⇒":
+                        eval_stack.insert(0, f"{current[-2]} ⇒ {current[-1]}")
+                    case "⇔":
+                        eval_stack.insert(0, f"{current[-2]} ⇔ {current[-1]}")
+
+                current = current[0:-2]
+
+            if not eval_stack[0] in subexps:
+                subexps[eval_stack[0]] = op_count
+            op_count += 1
+
+    if not list_format:
+        subexps = {subexps[subexp]: subexp for subexp in subexps}
+        return subexps
+    else:
+        return list(subexps.keys())
+
+
 def evaluate_postfix(exp: str, truth_values: dict) -> bool:
     """Takes a postfix (RPN) expression and evaluates it, using `truth_values`
     to assign truth values to the prime propositions in the order that they
@@ -133,24 +185,25 @@ def evaluate_postfix(exp: str, truth_values: dict) -> bool:
 
     eval_stack = list(exp)
     current = []
+    subexps = get_subexps(exp)
+    intermediates = []
+    op_count = 0
 
-    # while there are still propositions left on the stack or there is an 
+    # while there are still propositions left on the stack or there is an
     # operator left to be applied to the propositions in `current`
-    while len(eval_stack) > 1 or is_operator(eval_stack[0]):
+    while len(eval_stack) > 0:
         if isinstance(eval_stack[0], bool):
             current.append(eval_stack.pop(0))
 
         elif is_proposition(eval_stack[0]):
             current.append(truth_values[eval_stack.pop(0)])
-        
+
         else:
             operator = eval_stack.pop(0)
-            # negation is a unary operator
-            # strictly speaking, this is a shortcut: i should have put the
-            # negation of the proposition back onto the stack and then
-            # pulled it back out but eh
+
             if operator == "¬":
-                current[-1] = not current[-1]
+                eval_stack.insert(0, not current[-1])
+                current = current[0:-1]
             else:
                 match operator:
                     case "∧":
@@ -161,13 +214,27 @@ def evaluate_postfix(exp: str, truth_values: dict) -> bool:
                         eval_stack.insert(0, (not current[-2]) or current[-1])
                     case "⇔":
                         eval_stack.insert(0, current[-2] == current[-1])
-                
-                current = current[0: -2]
-    
-    return eval_stack[0]
+
+                current = current[0:-2]
+
+            if op_count in subexps:
+                intermediates.append(eval_stack[0])
+
+            op_count += 1
+
+    return intermediates
+
 
 def main():
-    print(produce_truth_combinations(*(count_identifiers("¬p ∧ q ∨ ¬q ∧ r"))))
+    # print(produce_truth_combinations(*(count_identifiers("¬p ∧ q ∨ ¬q ∧ r"))))
+    # print(get_subexps(shunting_yard("¬(p ∧ q) ∨ ¬(p ∧ q)")))
+    print(
+        evaluate_postfix(
+            exp=shunting_yard("p ∧ (q ∨ r) ⇔ (p ∧ q) ∨ (p ∧ r)"),
+            truth_values={"p": True, "q": False, "r": True},
+        )
+    )
+
 
 if __name__ == "__main__":
     main()
--- main.py
+++ main.py
@@ -1,34 +1,72 @@
-from lib import shunting_yard, evaluate_postfix, count_identifiers, produce_truth_combinations
+from lib import (
+    shunting_yard,
+    evaluate_postfix,
+    count_identifiers,
+    produce_truth_combinations,
+    get_subexps,
+)
+
+import sys
+import csv
 
 # ASCII escape codes to print the truth values in colour
 RED = "\033[0;31m"
 GREEN = "\033[0;32m"
 ENDCOLOUR = "\033[0m"
 
+
 def main():
-    exp = "(p ∨ q) ∧ (p ∨ r)"
+    if len(sys.argv) == 2:
+        exp = sys.argv[1]
+        output_to_file = False
+    elif len(sys.argv) == 4 and sys.argv[1] == "-o":
+        exp = sys.argv[3]
+        output_to_file = True
+        file_name = sys.argv[2]
 
     postfix = shunting_yard(exp)
     count, identifiers = count_identifiers(exp)
     truth_combinations = produce_truth_combinations(count, identifiers)
+    subexps = get_subexps(postfix, list_format=True)
 
-    for identifier in identifiers:
-        print(identifier, end="\t")
+    if output_to_file:
+        with open(file_name, "w", newline="", encoding="utf-8") as csv_file:
+            ttable_writer = csv.writer(csv_file)
 
-    print("| Output")
-    print("-" * 8 * (count + 1))
+            ttable_writer.writerow(identifiers + subexps)
 
-    for truth_combination in truth_combinations:
-        for identifier in truth_combination:
-            if truth_combination[identifier]:
-                print("T", end="\t")
+            for truth_combination in truth_combinations:
+                row = [
+                    "T" if truth_combination[identifier] else "F"
+                    for identifier in truth_combination
+                ]
+                row.extend(
+                    [
+                        "T" if val else "F"
+                        for val in evaluate_postfix(postfix, truth_combination)
+                    ]
+                )
+
+                ttable_writer.writerow(row)
+    else:
+        for identifier in identifiers:
+            print(identifier, end="\t")
+
+        print(f"| Output")
+        print("-" * 8 * (count + 1))  # tab width is 8, so 8 "-"s for every tab
+
+        for truth_combination in truth_combinations:
+            for identifier in truth_combination:
+                if truth_combination[identifier]:
+                    print("T", end="\t")
+                else:
+                    print("F", end="\t")
+
+            if evaluate_postfix(postfix, truth_combination)[-1]:
+                print("| " + GREEN + "T" + ENDCOLOUR)
             else:
-                print("F", end="\t")
+                print("| " + RED + "F" + ENDCOLOUR)
 
-        if evaluate_postfix(postfix, truth_combination):
-            print("| " + GREEN + "T" + ENDCOLOUR)
-        else:
-            print("| " + RED + "F" + ENDCOLOUR)
-    
+
 if __name__ == "__main__":
     main()
